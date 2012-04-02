d8a8a92bb24e6b2072ac640f77407139a4cec005
Christian Kauhaus <kc@gocept.com>
2012-04-02 13:23:39 +0200

    Fix packaging glitches

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def project_path(*names):
     return os.path.join(os.path.dirname(__file__), *names)
 
 
-version = '0.2.dev0'
+version = '0.1p1'
 
 setup(
     name='batou',
@@ -39,7 +39,7 @@ setup(
     """,
     author='gocept <mail@gocept.com>',
     author_email='mail@gocept.com',
-    license='BSD',
+    license='ZPL',
     url='https://projects.gocept.com/projects/batou/',
     keywords='deployment',
     classifiers="""\
