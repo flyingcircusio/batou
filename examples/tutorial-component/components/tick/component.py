4c0c08ef0e217e470c395cf6e13492a7da600590
Thomas Lotze <164362+tlotze@users.noreply.github.com>
2012-07-05 19:21:35 +0200

    fixed the component definition, added the environment definition to the tutorial

--- examples/tutorial-component/components/tick/component.py
+++ examples/tutorial-component/components/tick/component.py
@@ -4,6 +4,6 @@ from batou.lib.file import File
 class Tick(Component):
 
     def configure(self):
-        self += File('hello', source='tick.sh', mode=0755)
+        self += File('tick.sh', source='tick.sh', mode=0755)
         self.provide('programs',
                      self.expand('10 tick {{component.workdir}}/tick.sh true'))
