b1e42e7cf39060606a6fb65b0d29483edecbfcd2
Christian Theune <ct@flyingcircus.io>
2012-07-05 16:02:47 +0200

    Typo.

--- src/batou/lib/buildout.py
+++ src/batou/lib/buildout.py
@@ -37,7 +37,7 @@ class Buildout(Component):
         if self.config is None:
             self.config = File('buildout.cfg',
                                source='buildout.cfg',
-                               template=True)
+                               is_template=True)
         if isinstance(self.config, Component):
             self.config = [self.config]
         for component in self.config:
