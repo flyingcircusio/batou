03873802078a9a7d0ded8df5690464cb3de2bf96
Christian Theune <ct@gocept.com>
2012-07-06 10:27:45 +0200

    Enable service integration for component tutorial.

--- examples/tutorial-component/components/supervisor/component.py
+++ examples/tutorial-component/components/supervisor/component.py
@@ -1,11 +1,14 @@
 from batou.component import Component
 from batou.lib.buildout import Buildout
+from batou.lib.service import Service
 
 class Supervisor(Component):
 
     def configure(self):
         self.programs = self.require('programs', self.host)
         self += Buildout('supervisor', python='2.7')
+        self += Service('bin/supervisord',
+                        pidfile='var/supervisord.pid')
 
     def verify(self):
         self.assert_file_is_current(
