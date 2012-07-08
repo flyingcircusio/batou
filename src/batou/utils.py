5b00e71f9d483bf07e30b849ac061b00a8703f52
Christian Theune <ct@flyingcircus.io>
2012-07-08 21:25:44 +0200

    Try improving debugging log.

--- src/batou/utils.py
+++ src/batou/utils.py
@@ -21,7 +21,7 @@ class MultiFile(object):
 
     def write(self, value):
         for file in self.files:
-            file.write('-> '+value)
+            file.write(value)
 
     def flush(self):
         for file in self.files:
@@ -30,7 +30,7 @@ class MultiFile(object):
     def read(self, count=None):
         value = self.files[0].read()
         for file in self.files[1:]:
-            file.write('<- '+value)
+            file.write(value)
         return value
 
 
