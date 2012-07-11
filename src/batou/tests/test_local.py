f150ff25e4a5867db020358a1e958b144fb6a984
Christian Theune <ct@flyingcircus.io>
2012-07-11 18:47:27 +0200

    Add test coverage for batch mode.

--- src/batou/tests/test_local.py
+++ src/batou/tests/test_local.py
@@ -1,12 +1,13 @@
+from StringIO import StringIO
+from batou.local import main, AutoMode, BatchMode
+from mock import patch, Mock
 import unittest
-import mock
-from batou.local import main, AutoMode
 
 
 class LocalTests(unittest.TestCase):
 
-    @mock.patch('sys.argv')
-    @mock.patch('sys.stderr')
+    @patch('sys.argv')
+    @patch('sys.stderr')
     def test_no_args_exits_with_error(self, stderr, argv):
         with self.assertRaises(SystemExit):
             main()
@@ -25,19 +26,83 @@ class TestComponent(object):
 class AutoModeTests(unittest.TestCase):
 
     def test_no_components_no_cry(self):
-        environment = mock.Mock()
+        environment = Mock()
         environment.ordered_components = []
         mode = AutoMode(environment, 'asdf')
         mode()
 
     def test_components_with_same_host_get_deployed_in_order(self):
-        environment = mock.Mock()
-        host = mock.Mock()
+        environment = Mock()
+        host = Mock()
         environment.get_host.return_value = host
         log = []
         environment.ordered_components = components = [
                 TestComponent(log, host),
-                TestComponent(log, mock.Mock()),
+                TestComponent(log, Mock()),
                 TestComponent(log, host)]
-        AutoMode(environment, 'asdf')()
+        AutoMode(environment, 'localhost')()
         self.assertListEqual([components[0], components[2]], log)
+
+
+class BatchModeTests(unittest.TestCase):
+
+    def test_set_command_configures_environment(self):
+        environment = Mock()
+        environment.overrides = {}
+        mode = BatchMode(environment, 'asdf')
+        mode.cmd_set('foo bar baz')
+        self.assertEquals({'foo': {'bar': 'baz'}}, environment.overrides)
+
+    def test_set_command_configures_overrides_for_environment(self):
+        environment = Mock()
+        environment.overrides = {}
+        mode = BatchMode(environment, 'localhost')
+        mode.cmd_set('foo bar baz')
+        self.assertEquals({'foo': {'bar': 'baz'}}, environment.overrides)
+
+    def test_configure_command_configures_environment(self):
+        environment = Mock()
+        mode = BatchMode(environment, 'localhost')
+        mode.cmd_configure()
+        self.assertTrue(environment.configure.called)
+
+    def test_deploy_command_deploys_named_component(self):
+        environment = Mock()
+        log = []
+        mode = BatchMode(environment, 'localhost')
+        mode.host = dict(asdf=TestComponent(log, Mock()),
+                         bsdf=TestComponent(log, Mock()))
+        mode.cmd_deploy('asdf')
+        self.assertListEqual([mode.host['asdf']], log)
+
+    @patch('batou.utils.input')
+    def test_batchmode_input_triggers_and_output(self, input):
+        environment = Mock()
+        mode = BatchMode(environment, 'localhost')
+        mode.output = StringIO()
+
+        def input_mock(prompt):
+            try:
+                value = input_script.pop(0)
+            except IndexError:
+                raise EOFError
+            return value
+
+        input_script = ['set component key value',
+                        'configure',
+                        'deploy component',
+                        '',
+                        'asdf']
+        mode.input = input_mock
+        mode.cmd_set = Mock()
+        mode.cmd_configure = Mock()
+        mode.cmd_deploy = Mock()
+        mode()
+        mode.cmd_set.assert_called_with('component key value')
+        mode.cmd_configure.assert_called_with()
+        mode.cmd_deploy.assert_called_with('component')
+
+        mode.cmd_set.side_effect = Exception('Failed set')
+        input_script = ['set asdf']
+        mode()
+        self.assertEquals('OK\nOK\nOK\nERROR\n', mode.output.getvalue())
