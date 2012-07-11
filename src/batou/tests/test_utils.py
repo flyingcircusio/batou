e7ceaac313bc92b1c82393971a998176836e356c
Christian Theune <ct@gocept.com>
2012-07-11 16:28:08 +0200

    Add test coverage for our custom input/prompt utility function.

--- src/batou/tests/test_utils.py
+++ src/batou/tests/test_utils.py
@@ -1,5 +1,7 @@
-from batou.utils import resolve
+from StringIO import StringIO
+from batou.utils import resolve, input
 import mock
+import sys
 import unittest
 
 
@@ -14,3 +16,13 @@ class ResolveTests(unittest.TestCase):
     def test_host_with_port_resolves_and_keeps_port(self, ghbn):
         ghbn.return_value = '127.0.0.1'
         self.assertEqual('127.0.0.1:8080', resolve('localhost:8080'))
+
+
+class InputTests(unittest.TestCase):
+
+    @mock.patch('__builtin__.raw_input')
+    def test_input(self, raw_input):
+        raw_input.return_value = 'asdf'
+        out = StringIO()
+        self.assertEquals('asdf', input('foo', out))
+        self.assertEquals('foo', out.getvalue())
