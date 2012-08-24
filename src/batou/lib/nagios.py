d65f817b9af3799fa5bb2095ef104225bf4504a8
Christian Theune <ct@flyingcircus.io>
2012-08-24 10:13:02 +0200

    Start working on the nagios components.

--- src/batou/lib/nagios.py
+++ src/batou/lib/nagios.py
@@ -1,18 +1,53 @@
-from batou.component import Component
+from batou.component import Component, HookComponent
 
 
-class Check(Component):
+class Check(HookComponent):
 
     namevar = 'description'
+    key = 'batou.lib.nagios:Check'
 
-    type = 'nrpe'
-    host = None
+    args = ''
     command = None  # path to executable, if relative then to compdir
-    args = None
+    host = None
     name = None  # by default derived automatically from description
+    notes_url = ''
+    type = 'nrpe'
+
+    # dependencies are a list of (host_object, check_description)
+    depend_on = ()
 
 
-class Nagios(Component):
+class HTTPCheck(Check):
+
+    namevar = 'url'
+
+    warning = 4
+    critical = 8
+    timeout = 30
 
     def configure(self):
-        pass
+        if not self.description:
+            self.description = self.url
+
+        url = urlparse.urlparse(check.url)
+        self.ssl = dict(
+            http='',
+            https='-S')[url.scheme]
+        self.httphost = url.netloc
+        self.urlpath = url.path
+
+
+
+class NagiosServer(Component):
+    """Generate a Nagios server config snippet that reflects all active checks.
+
+    The generated config needs to be copied to the Nagios server.
+
+    """
+
+    def configure(self):
+        self.checks = self.require(Check.key)
+        self += File('nagios.cfg',
+                     source='nagios.cfg',
+                     mode=0o644,
+                     is_template=True)
