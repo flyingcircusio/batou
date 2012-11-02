d922c274c822d737932cd3e921a834c1f8897086
Christian Theune <ct@gocept.com>
2012-11-02 10:22:53 +0100

    Switch to regular Sphinx setup to allow switching to readthedocs.

--- doc/conf.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) 2012 gocept gmbh & co. kg
-# See also LICENSE.txt
-
-_year_started = 2012
-
-import gocept.package.sphinxconf
-autosummary_generate = True
-gocept.package.sphinxconf.set_defaults()
