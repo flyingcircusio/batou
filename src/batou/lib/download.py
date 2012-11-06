333bfde0d6fedef3bf9c7c09fd5c01e1737b8cad
Christian Theune <ct@gocept.com>
2012-11-06 08:46:51 +0100

    Internalize the hashing of downloads using Python's hashlib.

--- src/batou/lib/download.py
+++ src/batou/lib/download.py
@@ -1,5 +1,5 @@
 from batou.component import Component
-from batou.utils import md5sum
+from batou.utils import hash
 import batou
 import os.path
 import urlparse
@@ -10,7 +10,8 @@ class Download(Component):
     namevar = 'uri'
 
     target = None  # Filename where the download will be stored.
-    md5sum = None
+    md5sum = None  # XXX deprecated
+    checksum = None
 
     def configure(self):
         if not self.target:
@@ -18,16 +19,17 @@ class Download(Component):
         if not self.target:
             raise KeyError('No target is given and the URI does not allow '
                            'deriving a filename.')
-        if not self.md5sum:
-            raise KeyError('No MD5 sum for download given.')
+        if self.md5sum:
+            raise ValueError('md5sum is deprecated. Use checksum="md5:..."')
+        self.checksum_function, self.checksum = self.checksum.split(':')
 
     def verify(self):
         if not os.path.exists(self.target):
             raise batou.UpdateNeeded()
-        if self.md5sum != md5sum(self.target):
+        if self.checksum != hash(self.target):
             raise batou.UpdateNeeded()
 
     def update(self):
         self.cmd('wget -q -O {target} {uri}'.format(
                     target=self.target, uri=self.uri))
-        assert self.md5sum == md5sum(self.target)
+        assert self.checksum == hash(self.target, self.checksum_function)
