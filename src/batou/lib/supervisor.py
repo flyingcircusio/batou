2148673ef64fc2ef621c3ec3d855a8defecba995
Stefan Walluhn <4291979+stefan-walluhn@users.noreply.github.com>
2012-12-14 14:10:08 +0100

    logrotate branch

--- src/batou/lib/supervisor.py
+++ src/batou/lib/supervisor.py
@@ -156,7 +156,9 @@ class Supervisor(Component):
             python='2.7')
 
         self += Directory('var/log', leading=True)
-        self += RotatedLogfile('var/log/*.log')
+
+        postrotate = self.expand('kill -USR2 $({{component.workdir}}/bin/supervisorctl pid)')
+        self += RotatedLogfile('var/log/*.log', postrotate=postrotate)
 
         self += Service('bin/supervisord', pidfile='var/supervisord.pid')
 
