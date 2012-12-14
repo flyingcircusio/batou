2729134ebd6f44fc0c65f9e9ed366d8f5b768e53
Stefan Walluhn <4291979+stefan-walluhn@users.noreply.github.com>
2012-12-14 14:19:08 +0100

    backout

--- src/batou/lib/logrotate.py
+++ src/batou/lib/logrotate.py
@@ -7,13 +7,11 @@ class RotatedLogfile(HookComponent):
 
     namevar = 'path'
     key = 'osha.lib.logrotate:RotatedLogfile'
-
     args = ''
 
     def configure(self):
         super(RotatedLogfile, self).configure()
         self.path = os.path.join(self.workdir, self.path)
-        self.path = self.map(path)
         self.args = map(str.strip, self.args.split(','))
 
 
@@ -21,6 +19,7 @@ class Logrotate(Component):
 
     logrotate_template = os.path.join(
         os.path.dirname(__file__), 'resources', 'logrotate.in')
+
 
     def configure(self):
         self.logfiles = self.require(RotatedLogfile.key, host=self.host)
