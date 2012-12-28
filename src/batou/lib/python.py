b127f3e776be7ea75a793fb5f96aa15c78a70c5d
Christian Zagrodnick <cz@flyingcircus.io>
2012-12-28 12:27:59 +0100

    Use a version specific virtualenv, if available
    Recent versions of virtualenv are not compatible with python2.4. It's not
    really feasable to downgrade. Trying a specific version first will do the
    trick.

--- src/batou/lib/python.py
+++ src/batou/lib/python.py
@@ -24,6 +24,23 @@ class VirtualEnv(Component):
     def verify(self):
         self.assert_file_is_current(self.python)
 
+    def _detect_virtualenv(self):
+        # Prefer the virtualenv of the target version. There are some
+        # incompatibilities between Python and virtualenv versions.
+        possible_executables = ['virtualenv-{}'.format(self.version),
+                                'virtualenv']
+        for executable in possible_executables:
+            try:
+                self.cmd('type {}'.format(executable))
+            except RuntimeError:
+                pass
+            else:
+                break
+        else:
+            raise RuntimeError('Could not find virtualenv executable')
+        return executable
+
     def update(self):
-        self.cmd('virtualenv --no-site-packages --python python{} .'.format(
-                 self.version))
+        executable = self._detect_virtualenv()
+        self.cmd('{} --no-site-packages --python python{} .'.format(
+                 executable, self.version))
---
+++ src/batou/lib/tests/test_python.py
@@ -0,0 +1,39 @@
+# Copyright (c) 2012 gocept gmbh & co. kg
+# See also LICENSE.txt
+
+import mock
+import unittest
+
+
+class TestVirtualEnv(unittest.TestCase):
+
+    def virtualenv(self, *args, **kw):
+        from ..python import VirtualEnv
+        virtualenv = VirtualEnv(*args, **kw)
+        return virtualenv
+
+    def test_detect_should_use_version_specific_virtualenv_if_available(self):
+        virtualenv = self.virtualenv('7.25')
+        virtualenv.cmd = mock.Mock(return_value='')
+        self.assertEqual('virtualenv-7.25', virtualenv._detect_virtualenv())
+
+    def test_detect_should_use_non_version_spec_venv_if_no_specific_available(
+        self):
+        virtualenv = self.virtualenv('7.25')
+        virtualenv.cmd = mock.Mock(side_effect=[RuntimeError(), ''])
+        self.assertEqual('virtualenv', virtualenv._detect_virtualenv())
+
+    def test_detect_should_raise_RuntimeError_if_no_virtualenv_available(self):
+        virtualenv = self.virtualenv('7.25')
+        virtualenv.cmd = mock.Mock(side_effect=RuntimeError())
+        with self.assertRaises(RuntimeError):
+            virtualenv._detect_virtualenv()
+
+    def test_update_should_call_executable_provided_by_detect(self):
+        virtualenv = self.virtualenv('7.25')
+        virtualenv.cmd = mock.Mock()
+        virtualenv._detect_virtualenv = mock.Mock(
+            return_value='virtualenv-executable')
+        virtualenv.update()
+        virtualenv.cmd.assert_called_with(
+            'virtualenv-executable --no-site-packages --python python7.25 .')
