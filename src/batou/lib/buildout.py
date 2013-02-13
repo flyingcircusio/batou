12b1e983c72c485c89f615b4c0ba26f924f65bcd
Wolfgang Schnerring <wosc@wosc.de>
2013-02-13 08:22:25 +0100

    Use long option for better readibility

--- src/batou/lib/buildout.py
+++ src/batou/lib/buildout.py
@@ -27,7 +27,7 @@ class Bootstrap(Component):
             raise UpdateNeeded()
 
     def update(self):
-        self.cmd('%s bootstrap.py -d' % self.python)
+        self.cmd('%s bootstrap.py --distribute' % self.python)
 
 
 class Buildout(Component):
