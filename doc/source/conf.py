20f69b37db4f0a9a0d755c555977ac6be352ce93
Christian Theune <ct@flyingcircus.io>
2013-06-19 10:35:57 +0200

    Clean up and organize the documentation a bit more.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -41,7 +41,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'batou'
-copyright = u'2012, gocept gmbh & co. kg'
+copyright = u'2012-2013, gocept gmbh & co. kg'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
