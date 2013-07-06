80c6470e4e03273a3363cfc844cee361426620c6
Christian Theune <ct@gocept.com>
2013-07-06 14:17:49 +0200

    Implement secrets as environment overrides.
    Part 1: support local runner.

--- examples/tutorial-secrets/components/hello/component.py
+++ examples/tutorial-secrets/components/hello/component.py
@@ -4,8 +4,8 @@ from batou.lib.file import File
 
 class Hello(Component):
 
+    magic_word = None
+
     def configure(self):
-        secrets = self.require('secrets')[0]
-        magic_word = secrets.get('hello', 'magicword')
         self += File('hello',
-            content='The magic word is {}'.format(magic_word))
+            content='The magic word is {}.\n'.format(self.magic_word))
--- examples/tutorial-secrets/components/secrets/component.py
+++
@@ -1,4 +0,0 @@
-import batou.lib.secrets
-
-# Components are classes: it's enough to assign them a local name.
-Secrets = batou.lib.secrets.Secrets
