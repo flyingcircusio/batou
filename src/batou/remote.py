ef09f079d571fcd7cf33055d273cc587ca04384f
Christian Theune <ct@gocept.com>
2013-07-23 13:27:38 +0200

    Make bootstrapping virtualenvs for batou itself more reliable WRT choosing the right setuptools version.

--- src/batou/remote.py
+++ src/batou/remote.py
@@ -12,6 +12,9 @@ import subprocess
 import sys
 
 
+import pkg_resources
+MY_SETUPTOOLS_VERSION = pkg_resources.require("setuptools")[0].version
+
 logger = logging.getLogger('batou.remote')
 
 
@@ -203,7 +206,10 @@ class RemoteHost(object):
                 self._reset_before_bootstrap()
             self.cmd(u'hg update -C %s' % self.deployment.environment.branch)
             if not self.exists('bin/python2.7'):
-                self.cmd('virtualenv --no-site-packages --python python2.7 .')
+                self.cmd('virtualenv --no-site-packages --setuptools '
+                         '--python python2.7 .')
+            self.cmd('bin/pip install --upgrade setuptools=={}'.format(
+                MY_SETUPTOOLS_VERSION))
             if not self.exists('bin/buildout'):
                 # XXX We tend to have upgrade issues with setuptools. We used
                 # to always bootstrap but it's becoming a pain.
