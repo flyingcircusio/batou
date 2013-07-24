a93c5f7978ea063acf0f8070227600190f7c9039
Christian Theune <ct@flyingcircus.io>
2013-07-24 14:16:43 +0200

    Also reinstall virtualenv if pip is broken ... *siiiiiigh*

--- src/batou/component.py
+++ src/batou/component.py
@@ -288,6 +288,7 @@ class Buildout(Component):
         else:
             try:
                 self.cmd('bin/python -c "import pkg_resources"')
+                self.cmd('bin/pip --help')
             except subprocess.CalledProcessError:
                 self.cmd('rm -rf bin/ lib/ include/')
                 do_install_venv = True
