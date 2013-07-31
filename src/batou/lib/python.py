c88e7b2349f6e6ec0054b154776d7f43d4d490df
Christian Zagrodnick <cz@gocept.com>
2013-07-31 17:24:04 +0200

    actually update package

--- src/batou/lib/python.py
+++ src/batou/lib/python.py
@@ -130,6 +130,7 @@ class Package(Component):
                     base_package), silent=True)
         except RuntimeError:
             self.pip_install_options.extend(['-I', '--no-deps'])
+            raise UpdateNeeded()
 
     def update(self):
         options = ' '.join(self.pip_install_options)
