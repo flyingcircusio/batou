f4761b7c642a30a108c9ccecc33d1a124dcaf8f9
Christian Theune <ct@flyingcircus.io>
2013-08-16 13:28:01 +0200

    Improve bootstrapping, self-updating, self-installing code.

--- src/batou/__init__.py
+++ src/batou/__init__.py
@@ -1,12 +1,4 @@
-# Ensure platform components have a chance to register.
-
-# XXX make more flexible. E.g. by using entry points.
-import batou.lib.goceptnet  # noqa
-batou  # Make pyflakes happy
-# API re-exports
-from .component import Component  # noqa
-Component  # Make pyflakes happy
-
+# This code must not cause non-stdlib imports to support self-bootstrapping.
 
 class UpdateNeeded(Exception):
     """A component requires an update."""
