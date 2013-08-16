4f7c34fd9e247d2840220b601273362bd02c10b8
Wolfgang Schnerring <ws@gocept.com>
2013-08-16 19:25:50 +0200

    Print environment name, not <Environment...> repr

--- src/batou/local.py
+++ src/batou/local.py
@@ -21,9 +21,9 @@ def main(environment, hostname, platform):
             logger.exception(e)
             notify('Deployment failed',
                    '{}:{} encountered an error.'.format(
-                       environment, hostname))
+                       environment.name, hostname))
             sys.exit(1)
         else:
             notify('Deployment finished',
                    '{}:{} was deployed successfully.'.format(
-                       environment, hostname))
+                       environment.name, hostname))
