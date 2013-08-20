1cb619869ddce9c85199593b66b819e6a7c62dd9
Wolfgang Schnerring <wosc@wosc.de>
2013-08-20 14:20:07 +0200

    Formatting

--- src/batou/lib/python.py
+++ src/batou/lib/python.py
@@ -92,12 +92,10 @@ class Package(Component):
             self.timeout = self.environment.timeout
 
     def verify(self):
-        # Is the right version installed according to PIP?
         try:
             self.cmd(
                 'bin/python -c "import pkg_resources; '
-                'assert pkg_resources.require('
-                '\'{}\')[0].version == \'{}\'"'
+                'assert pkg_resources.require(\'{}\')[0].version == \'{}\'"'
                 .format(self.package, self.version), silent=True)
         except RuntimeError, e:
             logger.debug(e[3])
