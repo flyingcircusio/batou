ccf2d1b645594fdaf8ea355e45d354a6038d685c
Wolfgang Schnerring <wosc@wosc.de>
2013-09-27 09:47:30 +0200

    Remove tests that were obsoleted by commit:cd58579d29c1

--- src/batou/lib/tests/test_python.py
+++ src/batou/lib/tests/test_python.py
@@ -1,44 +1,6 @@
 from batou.lib.python import Package, VirtualEnv
 import mock
 import pytest
-import unittest
-
-
-class TestVirtualEnv(unittest.TestCase):
-
-    def virtualenv(self, *args, **kw):
-        virtualenv = VirtualEnv(*args, **kw)
-        return virtualenv
-
-    def test_detect_should_use_version_specific_virtualenv_if_available(self):
-        virtualenv = self.virtualenv('7.25')
-        virtualenv.cmd = mock.Mock(return_value='')
-        self.assertEqual(
-            'virtualenv-7.25 --no-site-packages',
-            virtualenv._detect_virtualenv())
-
-    def test_detect_should_use_non_version_spec_venv_if_no_specific_available(
-            self):
-        virtualenv = self.virtualenv('7.25')
-        virtualenv.cmd = mock.Mock(side_effect=[RuntimeError(), ''])
-        self.assertEqual(
-            'virtualenv --no-site-packages --python python7.25',
-            virtualenv._detect_virtualenv())
-
-    def test_detect_should_raise_RuntimeError_if_no_virtualenv_available(self):
-        virtualenv = self.virtualenv('7.25')
-        virtualenv.cmd = mock.Mock(side_effect=RuntimeError())
-        with self.assertRaises(RuntimeError):
-            virtualenv._detect_virtualenv()
-
-    def test_update_should_call_executable_provided_by_detect(self):
-        virtualenv = self.virtualenv('7.25')
-        virtualenv.cmd = mock.Mock()
-        virtualenv._detect_virtualenv = mock.Mock(
-            return_value='virtualenv-executable arguments')
-        virtualenv.update()
-        virtualenv.cmd.assert_called_with(
-            'virtualenv-executable arguments .')
 
 
 def test_package_install(root):
