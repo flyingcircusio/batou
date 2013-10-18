51731932358cfaafd8b0e9a8d7efd2c7994b90d7
Wolfgang Schnerring <wosc@wosc.de>
2013-10-18 16:55:24 +0200

    Add smoke test for Cron component

---
+++ src/batou/lib/tests/test_cron.py
@@ -0,0 +1,15 @@
+from ..cron import CronJob, CronTab
+import batou.vfs
+import os.path
+
+
+def test_collects_cronjobs_into_crontab(root):
+    root.environment.vfs_sandbox = batou.vfs.Developer(root.environment, None)
+    root.component += CronJob('command1')
+    root.component += CronJob('command2')
+    root.component += CronTab()
+    root.component.deploy()
+    crontab = open(os.path.join(
+        root.environment.workdir_base, 'mycomponent/crontab')).read()
+    assert 'command1' in crontab
+    assert 'command2' in crontab
