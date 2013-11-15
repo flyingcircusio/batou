1b98f31b47bd778f3f2def68ac0997563ff95233
Wolfgang Schnerring <ws@gocept.com>
2013-11-15 13:39:44 +0100

    Re #12418: Provide convenience supervisor component with useable pidfile path

--- src/batou/lib/debian.py
+++ src/batou/lib/debian.py
@@ -1,6 +1,7 @@
 from batou.component import Component, platform
 from batou.lib.cron import CronJob
 import batou.lib.service
+import batou.lib.supervisor
 
 
 @platform('debian', batou.lib.service.Service)
@@ -11,3 +12,9 @@ class RebootCronjob(Component):
             self.expand(
                 '{{component.root.workdir}}/{{component.parent.executable}}'),
             timing='@reboot', logger=self.root.name)
+
+
+# XXX can't use @platform since that's too late (see #12418)
+class Supervisor(batou.lib.supervisor.Supervisor):
+
+    pidfile = 'var/supervisord.pid'
