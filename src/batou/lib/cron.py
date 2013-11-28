0cb2f866b453774266934fb3f885d0b32a7cb345
Wolfgang Schnerring <ws@gocept.com>
2013-11-28 09:00:08 +0100

    Support multiple crontabs by making ``key`` and ``filename`` configurable

--- src/batou/lib/cron.py
+++ src/batou/lib/cron.py
@@ -1,7 +1,7 @@
 from batou import UpdateNeeded
 from batou.component import Component, HookComponent, platform
 from batou.lib.file import File
-import os
+import pkg_resources
 
 
 class CronJob(HookComponent):
@@ -32,15 +32,17 @@ def ignore_comments(data):
 
 class CronTab(Component):
 
-    crontab_template = os.path.join(
-        os.path.dirname(__file__), 'resources', 'crontab')
+    crontab_template = pkg_resources.resource_filename(
+        __name__, 'resources/crontab')
     mailto = None
+    filename = 'crontab'
+    key = CronJob.key
 
     def configure(self):
-        self.jobs = self.require(CronJob.key, host=self.host)
+        self.jobs = self.require(self.key, host=self.host)
         self.jobs.sort(key=lambda job: job.command + ' ' + job.args)
         self.crontab = File(
-            'crontab', source=self.crontab_template)
+            self.filename, source=self.crontab_template)
         self += self.crontab
 
 
