0d24aed4f641ac7b7a72d7296c24de9b6248c3f7
Christian Theune <ct@flyingcircus.io>
2015-06-19 09:57:28 +0200

    Add DNS to vagrant example for verification.

--- examples/vagrant/components/test/component.py
+++ examples/vagrant/components/test/component.py
@@ -1,8 +1,11 @@
-from batou.component import Component
+from batou.utils import Address
+from batou.component import Component, Attribute
 from batou.lib.file import File
 
 
 class Test(Component):
 
+    address = Attribute(Address, 'default:8080')
+
     def configure(self):
-        self += File('test', content='asdf')
+        self += File('test', content='asdf {{component.address.listen}}')
