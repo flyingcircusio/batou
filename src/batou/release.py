843f9655a0b344e35b1f6a3a7a35c0f0c10c529d
Christian Theune <ct@flyingcircus.io>
2016-03-09 19:21:03 +0100

    Fix zest.releaser integration.

--- src/batou/release.py
+++ src/batou/release.py
@@ -1,4 +1,4 @@
-from zest.releaser.utils import system
+from zest.releaser.utils import execute_command
 import logging
 import os
 
@@ -8,6 +8,6 @@ logger = logging.getLogger(__name__)
 def update_requirements(data):
     os.chdir(data['workingdir'])
     logger.info('Running buildout to update requirements.txt.')
-    system('bin/buildout')
+    execute_command('bin/buildout')
     logger.info('Committing requirements.txt.')
-    system('hg commit -v -m "Update requirements.txt"')
+    execute_command('hg commit -v -m "Update requirements.txt"')
