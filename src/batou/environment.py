222cb64d2e04a4aa739ae12c2f8103cb09396287
Andreas Zeidler <az@zitc.de>
2017-12-18 13:01:36 +0100

    Add workaround for Vagrant images not containing virtualenv.
    Batou needs virtualenv to be installed, but most upstream images used by
    kitchen/vagrant don't have it.  This change allow to run an arbitrary
    command after `kitchen create`, e.g.
    
      $ export BATOU_POST_KITCHEN_CREATE_CMD="apt-get install -y python-virtualenv"

--- src/batou/environment.py
+++ src/batou/environment.py
@@ -306,6 +306,9 @@ class Environment(object):
         elif self.connect_method == 'kitchen':
             output.step("kitchen", "Ensuring machines are up ...")
             cmd('kitchen create')
+            if 'BATOU_POST_KITCHEN_CREATE_CMD' in os.environ:
+                cmd("kitchen exec -c '{}'".format(
+                    os.environ['BATOU_POST_KITCHEN_CREATE_CMD']))
 
     # Deployment API (implements the configure-verify-update cycle)
 
