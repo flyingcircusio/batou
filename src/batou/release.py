907a8121cd93bc621134e1bbaec30c221df6e06a
Christian Zagrodnick <cz@flyingcircus.io>
2018-08-16 10:49:53 +0200

    Adapt for updated zest.releaser

--- src/batou/release.py
+++ src/batou/release.py
@@ -8,6 +8,6 @@ logger = logging.getLogger(__name__)
 def update_requirements(data):
     os.chdir(data['workingdir'])
     logger.info('Running buildout to update requirements.txt.')
-    execute_command('bin/buildout')
+    execute_command(['bin/buildout'])
     logger.info('Committing requirements.txt.')
-    execute_command('hg commit -v -m "Update requirements.txt"')
+    execute_command(['hg', 'commit', '-v', '-m', 'Update requirements.txt'])
