38730aa8ab0e6826e157f0afe46af096174e82d8
Christian Theune <ct@flyingcircus.io>
2019-10-10 14:45:35 +0200

    multiple python version testing support, use python 2.7 only for this branch

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,7 @@
 set -ex
 
 rm -rf .Python bin lib include
-virtualenv --python python2.7 .
+virtualenv --python python .
 bin/pip install setuptools==40.0.0
 bin/pip install zc.buildout==2.13.1
 ./bin/buildout bootstrap
