4f950c78e85ee95db38bae0f7b5f9d95ba1b2479
Sebastian Wehrmann <sebastian@wehrmann.de>
2019-10-10 10:04:38 +0200

    Add convencience function to get active components for a host.

--- src/batou/tests/test_environment.py
+++ src/batou/tests/test_environment.py
@@ -246,6 +246,15 @@ data-alias = baz
     assert 'baz' == e.hosts['foo'].data['alias']
 
 
+def test_components_for_host_can_be_retrieved_from_environment(sample_service):
+    e = Environment('test-overlapping-components')
+    e.load()
+    assert ['hello1', 'hello2'] == e.components_for(host='localhost')
+    assert ['hello3'] == e.components_for(host='other')
+    assert ['hello2', 'hello3'] == e.components_for(host='this')
+    assert [] == e.components_for(host='foobar')
+
+
 @mock.patch('batou.remote_core.Output.line')
 def test_log_in_component_configure_is_put_out(
         output, sample_service):
