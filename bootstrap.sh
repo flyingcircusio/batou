02595a27fc4fbcc008cda4549738809d411ac911
Christian Theune <ct@flyingcircus.io>
2019-10-12 11:14:03 +0200

    still fighting travis ...

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,6 +7,6 @@ PYTHON=$(basename $PYTHONABS)
 
 rm -rf .Python bin lib include
 $PYTHONABS -m venv .
-bin/$PYTHON -m pip install pip
+bin/$PYTHON -m pip install -I pip
 bin/pip install --upgrade setuptools pip zc.buildout
 ./bin/buildout
