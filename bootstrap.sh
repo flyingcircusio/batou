9dc38b677f999da26a967bf43a7816f7eac0df79
Christian Theune <ct@flyingcircus.io>
2019-10-12 10:33:04 +0200

    Generalize bootstrapping and make travis actually use the desired Python version.

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,8 +2,10 @@
 
 set -ex
 
+PYTHON=${1:-python3}
+
 rm -rf .Python bin lib include
-python3.5 -m venv .
-bin/python3.5 -m pip install pip
+$PYTHON -m venv .
+bin/$PYTHON -m pip install pip
 bin/pip install --upgrade setuptools pip zc.buildout
 ./bin/buildout
