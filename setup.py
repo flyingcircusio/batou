dff74e4c069fac88d1070b077ff4a113067ef702
Christian Theune <ct@flyingcircus.io>
2021-05-21 13:44:46 +0200

    Officially drop support for Python 3.5.
    Fixes #114

--- setup.py
+++ setup.py
@@ -4,9 +4,10 @@
 and deployments.
 """
 
-from setuptools import setup, find_packages
 import glob
 import os.path
+
+from setuptools import find_packages, setup
 
 
 def project_path(*names):
@@ -53,7 +54,6 @@ setup(
 License :: OSI Approved :: BSD License
 Programming Language :: Python
 Programming Language :: Python :: 3
-Programming Language :: Python :: 3.5
 Programming Language :: Python :: 3.6
 Programming Language :: Python :: 3.7
 Programming Language :: Python :: 3.8
@@ -73,5 +73,5 @@ Programming Language :: Python :: 3 :: Only
     )],
     zip_safe=False,
     test_suite="batou.tests",
-    python_requires=">=3.5",
+    python_requires=">=3.6",
 )
