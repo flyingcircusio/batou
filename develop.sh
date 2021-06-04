0c93e9d8b3b5a01c845bba43b4481e3d764b63bf
Michael Howitz <mh@gocept.com>
2021-06-04 11:16:05 +0200

    Ensure endtoend tests use correct Python version.
    Before this commit the newest usable Python version was used.
    This locally leads to test failures in examples/error* when Python 3.10
    is installed as this version has different error messages.
    
    Additionally fix the name of the created environment in examples
    which is now named `.appenv`.

--- develop.sh
+++ develop.sh
@@ -2,7 +2,7 @@
 
 set -ex
 
-rm -rf .Python bin lib include .tox examples/*/.batou
+rm -rf .Python bin lib include .tox examples/*/.appenv
 
 python3 -m venv .
 bin/pip install --upgrade -r requirements-dev.txt
