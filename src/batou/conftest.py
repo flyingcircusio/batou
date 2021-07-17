9b8b0a6589421475ab653b098892fff800516c3f
Christian Theune <ct@flyingcircus.io>
2021-07-17 10:27:21 +0200

    Fix re-encryption of fixture files and provide script to automate re-encryption.

--- src/batou/conftest.py
+++ src/batou/conftest.py
@@ -4,6 +4,6 @@ import os.path
 
 @pytest.fixture(autouse=True)
 def ensure_gpg_homedir(monkeypatch):
-    home = FIXTURE = os.path.join(
+    home = os.path.join(
         os.path.dirname(__file__), 'secrets', 'tests', 'fixture', 'gnupg')
     monkeypatch.setitem(os.environ, "GNUPGHOME", home)
