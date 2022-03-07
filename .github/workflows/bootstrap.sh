976f08a462a2650341575f24e03d3d04b0a3cb2b
Christian Theune <ct@flyingcircus.io>
2022-03-07 12:08:51 +0100

    ensure bootstrap test errors are visible

--- .github/workflows/bootstrap.sh
+++ .github/workflows/bootstrap.sh
@@ -1,8 +1,9 @@
 #/usr/bin/env bash
 # Sample as shown in https://batou.readthedocs.io/en/stable/user/install.html#starting-a-new-batou-project
-set -x
+set -ex
+base=$(cwd)
 mkdir myproject
 cd myproject
 git init
 # Extract current bootstrap command from the docs.
-grep  "bootstrap | sh" doc/source/user/install.txt | sed -e  's/\$//' | sh
+grep  "bootstrap | sh" $base/doc/source/user/install.txt | sed -e  's/\$//' | sh
