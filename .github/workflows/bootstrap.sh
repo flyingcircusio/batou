57ef7b47d0908094990c3669f5870be051b29b4f
Christian Theune <ct@flyingcircus.io>
2022-03-07 12:09:54 +0100

    typo

--- .github/workflows/bootstrap.sh
+++ .github/workflows/bootstrap.sh
@@ -1,7 +1,7 @@
 #/usr/bin/env bash
 # Sample as shown in https://batou.readthedocs.io/en/stable/user/install.html#starting-a-new-batou-project
 set -ex
-base=$(cwd)
+base=$(pwd)
 mkdir myproject
 cd myproject
 git init
