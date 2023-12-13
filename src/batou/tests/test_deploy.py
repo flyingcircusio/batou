954b96cc80e2dc39e08782ca5bf7ca4697fa9da2
Christian Theune <ct@flyingcircus.io>
2023-12-13 10:14:03 +0100

    Merge pull request #414 from flyingcircusio/147-improve-error-message
    Add error handling for "no host found in environment"

--- src/batou/tests/test_deploy.py
+++ src/batou/tests/test_deploy.py
@@ -60,3 +60,39 @@ ERROR: Secrets section for unknown component found
 ======================= DEPLOYMENT FAILED (during load) ========================
 """
     )  # noqa: E501 line too long
+
+
+def test_main_fails_if_no_host_in_environment(capsys):
+    os.chdir("examples/errorsnohost")
+
+    from batou.deploy import main
+
+    with pytest.raises(SystemExit) as r:
+        main(
+            environment="errorsnohost",
+            platform=None,
+            timeout=None,
+            dirty=False,
+            consistency_only=False,
+            predict_only=False,
+            jobs=None,
+            provision_rebuild=False,
+        )
+
+    assert r.value.code == 1
+
+    out, err = capsys.readouterr()
+    assert err == ""
+    assert out == Ellipsis(
+        """\
+batou/2... (cpython 3...)
+================================== Preparing ===================================
+main: Loading environment `errorsnohost`...
+main: Verifying repository ...
+main: Loading secrets ...
+================== Connecting hosts and configuring model ... ==================
+
+ERROR: No host found in environment.
+====================== DEPLOYMENT FAILED (during connect) ======================
+"""
+    )  # noqa: E501 line too long
