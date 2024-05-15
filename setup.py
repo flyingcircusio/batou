d14d723a4a4fe12964701a43b45f48a8bae22545
Eli Kogan-Wang <elikowa@gmail.com>
2024-05-15 08:44:48 +0200

    Remove comment for outdated jinja

--- setup.py
+++ setup.py
@@ -13,8 +13,6 @@ setup(
     version=version,
     install_requires=[
         "ConfigUpdater>=3.0.1",
-        # As long as we support 3.6 we're stuck here, to avoid
-        # https://stackoverflow.com/questions/67659135/flask-token-object-has-no-attribute-test-render-template-error
         "Jinja2>=3.1.4",
         "requests",
         # ConfigUpdater does not manage its minimum requirements correctly.
