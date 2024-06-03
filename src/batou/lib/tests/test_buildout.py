9c5dee7c1ff91d769d5ec1a8f3f0219ae41660c5
Eli Kogan-Wang <elikowa@gmail.com>
2024-06-03 14:52:41 +0200

    Update pip version in test_buildout.py

--- src/batou/lib/tests/test_buildout.py
+++ src/batou/lib/tests/test_buildout.py
@@ -72,7 +72,7 @@ def test_runs_buildout3_successfully(root, output):
         version="3.0.0b1",
         setuptools="54.1.1",
         wheel="0.36.2",
-        pip="24.0",
+        pip="21.0.1",
         config=File(
             "buildout3.cfg",
             source=str(pathlib.Path(__file__).parent / "buildout3-example.cfg"),
