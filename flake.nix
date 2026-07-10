6f4a8da625aed8e8867c540122a85129017c90c4
Philipp Herzog <ph@flyingcircus.io>
2026-07-10 10:11:13 +0200

    wip: add mercurial for testing

--- flake.nix
+++ flake.nix
@@ -9,8 +9,9 @@
     nix-filter.url = "github:numtide/nix-filter";
   };
 
-  outputs = inputs @ {flake-parts, ...}:
-    flake-parts.lib.mkFlake {inherit inputs;} {
+  outputs =
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
       imports = [
         inputs.treefmt-nix.flakeModule
       ];
@@ -20,52 +21,60 @@
         "aarch64-darwin"
       ];
 
-      perSystem = {
-        config,
-        pkgs,
-        ...
-      }: let
-        pyproject = builtins.fromTOML (builtins.readFile "${inputs.self}/pyproject.toml");
-        version = pyproject.project.version;
-        src = inputs.nix-filter.lib {
-          root = inputs.self;
-          exclude = [
-            (inputs.nix-filter.lib.matchExt "nix")
-            "flake.lock"
-          ];
-        };
-        batou = pkgs.python3Packages.callPackage ./nix/batou.nix {inherit src version;};
-        tox-uv = pkgs.python3Packages.callPackage ./nix/tox-uv.nix {};
-      in {
-        treefmt = {
-          projectRootFile = "flake.nix";
-          programs.alejandra.enable = true;
-          settings.formatter.alejandra.excludes = [
-            "src/batou/*"
-          ];
-          flakeCheck = false;
-        };
+      perSystem =
+        {
+          config,
+          pkgs,
+          ...
+        }:
+        let
+          pyproject = builtins.fromTOML (builtins.readFile "${inputs.self}/pyproject.toml");
+          version = pyproject.project.version;
+          src = inputs.nix-filter.lib {
+            root = inputs.self;
+            exclude = [
+              (inputs.nix-filter.lib.matchExt "nix")
+              "flake.lock"
+            ];
+          };
+          batou = pkgs.python3Packages.callPackage ./nix/batou.nix { inherit src version; };
+          tox-uv = pkgs.python3Packages.callPackage ./nix/tox-uv.nix { };
+        in
+        {
+          treefmt = {
+            projectRootFile = "flake.nix";
+            programs.alejandra.enable = true;
+            settings.formatter.alejandra.excludes = [
+              "src/batou/*"
+            ];
+            flakeCheck = false;
+          };
 
-        formatter = config.treefmt.build.wrapper;
+          formatter = config.treefmt.build.wrapper;
 
-        packages = rec {
-          default = batou;
-          inherit batou tox-uv;
+          packages = rec {
+            default = batou;
+            inherit batou tox-uv;
+          };
+
+          checks = {
+            inherit batou;
+          };
+
+          devShells.default = pkgs.mkShell {
+            packages = [
+              (pkgs.python3.withPackages (ps: [
+                batou
+                tox-uv
+                ps.tox
+              ]))
+              pkgs.mercurial
+            ];
+
+            shellHook = ''
+              export APPENV_BASEDIR=$PWD
+            '';
+          };
         };
-
-        checks = {
-          inherit batou;
-        };
-
-        devShells.default = pkgs.mkShell {
-          packages = [
-            (pkgs.python3.withPackages (ps: [batou tox-uv ps.tox]))
-          ];
-
-          shellHook = ''
-            export APPENV_BASEDIR=$PWD
-          '';
-        };
-      };
     };
 }
